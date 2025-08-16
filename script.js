08f474773bd5dcc47a0fea936781f305ca08f68e
ErenYeager <eren.yeager.founding.titan.9@gmail.com>
2025-08-16 19:09:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-// Get ?id= from URL parameters
-const params = new URLSearchParams(window.location.search);
-const videoId = params.get("id");
+const servers = {
+  streamhg: "https://hglink.to/e/zqt5ix0dpggn",
+  abyss: "https://zplayer.io/?v=tFIl3ebsb"
+};
 
-// Select iframe
-const videoFrame = document.getElementById("videoFrame");
-
-if (videoId) {
-  // Set iframe source
-  videoFrame.src = `https://burnedplayer.pages.dev/?v=${encodeURIComponent(videoId)}`;
-} else {
-  // If no id in params, show message
-  document.querySelector(".player-container").innerHTML = `
-    <p style="color: red; font-size: 1.2rem;">
-      ❌ No video ID provided. Please use ?id= in the URL.
-    </p>
-  `;
+function loadServer(server) {
+  const frame = document.getElementById("videoFrame");
+  frame.src = servers[server];
 }
+
+// Auto-load default server
+window.onload = () => {
+  loadServer("streamhg");
+};
